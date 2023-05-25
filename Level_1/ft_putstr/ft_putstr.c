8632e05621a8dbadd0f657cd4039e96e4ba19141
Pierre Perol-Schneider <pierre.pascal.perol.2@gmail.com>
2023-05-25 15:00:05 +0200

    simplfied

--- Level_1/ft_putstr/ft_putstr.c
+++ Level_1/ft_putstr/ft_putstr.c
@@ -2,10 +2,14 @@
 
 void ft_putstr(char *str)
 {
-    int i = 0;
-    while (str[i])
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
+	int i = 0;
+	while (str[i])
+		write(1, &str[i++], 1);
+}
+
+int main(void)
+{
+	ft_putstr("Ceci est un test.");
+	write(1, "\n", 1);
+	return (0);
 }
