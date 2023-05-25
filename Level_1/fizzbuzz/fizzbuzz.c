fd51a615f9fe37d43925c4cfa9b67e50be009db7
Pierre Perol-Schneider <pierre.pascal.perol.2@gmail.com>
2023-05-25 14:42:04 +0200

    simplified

--- Level_1/fizzbuzz/fizzbuzz.c
+++ Level_1/fizzbuzz/fizzbuzz.c
@@ -1,28 +1,28 @@
 #include <unistd.h>
 
-void	ft_write_number(int number)
+void putnb(int nb)
 {
-	char	str[10] = "0123456789";
-
-	if (number > 9)
-		ft_write_number(number / 10);
-	write (1, &str[number % 10], 1);
+	char base[10] = "0123456789";
+	if (nb >= 10)
+		putnb(nb / 10);
+	write(1, &base[nb % 10], 1);
 }
 
-int		main()
+int main(void)
 {
 	int i = 1;
 	while (i <= 100)
 	{
 		if (i % 15 == 0)
-			write (1, "fizzbuzz", 8);
+			write(1, "fizzbuzz", 8);
 		else if (i % 3 == 0)
-			write (1, "fizz", 4);
+			write(1, "fizz", 4);
 		else if (i % 5 == 0)
-			write (1, "buzz", 4);
+			write(1, "buzz", 4);
 		else
-			ft_write_number(i);
+			putnb(i);
 		i++;
-		write (1, "\n", 1);
+		write(1, "\n", 1);
 	}
+	return (0);
 }
