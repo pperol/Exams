90f287b92925f67852af87f6a5ac2dc837ea8fd6
Pierre Perol-Schneider <pierre.pascal.perol.2@gmail.com>
2023-05-25 15:15:09 +0200

    add test.

--- Level_1/ft_strcpy/ft_strcpy.c
+++ Level_1/ft_strcpy/ft_strcpy.c
@@ -1,7 +1,8 @@
-char	*ft_strcpy(char *s1, char *s2)
+#include <unistd.h>
+
+char    *ft_strcpy(char *s1, char *s2)
 {
-	int i = 0;
-	
+	size_t i = 0;
 	while (s2[i])
 	{
 		s1[i] = s2[i];
@@ -10,3 +11,19 @@ char	*ft_strcpy(char *s1, char *s2)
 	s1[i] = '\0';
 	return (s1);
 }
+
+void ft_putstr(char *str)
+{
+	int i = 0;
+	while (str[i])
+		write(1, &str[i++], 1);
+}
+
+int main(void)
+{
+	char *test = "Ceci est un test";
+	char cpy[100];
+	ft_putstr(ft_strcpy(cpy, test));
+	return (0);
+}
+
