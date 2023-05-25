8d89db04dfe367fbe7c98d9db64369c47dce6d83
Pierre Perol-Schneider <pierre.pascal.perol.2@gmail.com>
2023-05-25 15:25:31 +0200

    add tests.

--- Level_1/ft_strcpy/ft_strcpy.c
+++ Level_1/ft_strcpy/ft_strcpy.c
@@ -12,18 +12,13 @@ char    *ft_strcpy(char *s1, char *s2)
 	return (s1);
 }
 
-void ft_putstr(char *str)
-{
-	int i = 0;
-	while (str[i])
-		write(1, &str[i++], 1);
-}
+#include <stdio.h>
 
 int main(void)
 {
-	char *test = "Ceci est un test";
+	char *test = "Ceci est un test.";
 	char cpy[100];
-	ft_putstr(ft_strcpy(cpy, test));
+	printf("%s\n", ft_strcpy(cpy, test));
 	return (0);
 }
 
