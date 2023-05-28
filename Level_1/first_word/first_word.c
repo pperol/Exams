486305da8f46f44a741bf9bba69a571af7c1d374
Pierre Perol-Schneider <pierre.pascal.perol.2@gmail.com>
2023-05-28 15:34:53 +0200

    corr

--- Level_1/first_word/first_word.c
+++ Level_1/first_word/first_word.c
@@ -6,9 +6,9 @@ int	main(int ac, char **av)
 
 	if (ac == 2)
 	{
-		while (av[1][i] == ' ' || av[1][i] == '\t')
+		while (av[1][i] && av[1][i] == ' ' || av[1][i] == '\t')
 			i++;
-		while (av[1][i] != ' ' && av[1][i] != '\t')
+		while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
 			write(1, &av[1][i++], 1);
 		write(1, "\n", 1);
 	}
